73996bb546178b3ce8b45540e386b7139bc2fa6d
archangel <adi.shr09@gmail.com>
2014-07-23 14:57:37 +0530

    Yolo

--- view_data.aspx.cs
+++ view_data.aspx.cs
@@ -14,43 +14,5 @@ public partial class Default2 : System.Web.UI.Page
     {
         SqlDataSource1.SelectCommand = @"select * from " + Request.QueryString["Stream"] + " where name='" + Session["username"] + "'and Year='"+Request.QueryString["Year"]+"'";
     }
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        string branch = DropDownList1.Text,shift = DropDownList3.Text;
-        int yr = Convert.ToInt32(DropDownList2.Text);
-        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\hph\Documents\Visual Studio 2013\WebSites\srekcufrehtoMdaB\App_Data\Database.mdf;Integrated Security=True");
-        conn.Open();
-        GridView1.DataBind();
-        if(branch=="CSE")
-        {
-            SqlCommand com1 = new SqlCommand(@"select * from CSE", conn);
-            r = com1.ExecuteReader();
-        }
-        else if(branch=="ECE")
-        {
-            SqlCommand com1 = new SqlCommand(@"select * from ECE", conn);
-            r = com1.ExecuteReader();
-        }
-        else if(branch=="EEE")
-        {
-            SqlCommand com1 = new SqlCommand(@"select * from EEE", conn);
-            r = com1.ExecuteReader();
-        }
-        else if(branch=="IT")
-        {
-            SqlCommand com1 = new SqlCommand(@"select * from IT", conn);
-            r = com1.ExecuteReader();
-        }
-        else if(branch=="ICE")
-        {
-            SqlCommand com1 = new SqlCommand(@"select * from ICE", conn);
-            r = com1.ExecuteReader();
-        }
-        while (r.Read())
-        {
-            Response.Write("Hello World!!\n\n");
-            Response.Write(String.Format("{0}\n{1}\n{2}\n{3}\n{4}\n{5}", r[0], r[1], r[2], r[3],r[4],r[5]));
-        }
-        conn.Close();
-    }
+   
 }
